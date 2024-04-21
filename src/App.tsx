4be511d2b26b2cf3c3e1a6d7d4af92172c193fe6
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-21 22:48:27 +0900

    Passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import ListGroupGoeun from "./components/ListGroupGoeun";
-
+import Alert from "./components/Alert";
 function App() {
-  let items = [
-    "Cheese in the Trap",
-    "Guardian: The Lonely and Great God",
-    "The King: Eternal Monarch",
-    "Yumis Cells",
-    "Yumis Cells S2",
-    "Little Women",
-  ];
-  let colors = [
-    "list-group-item-primary",
-    "list-group-item-secondary",
-    "list-group-item-success",
-    "list-group-item-danger",
-    "list-group-item-warning",
-    "list-group-item-info",
-  ];
+  
   return (
-    <div>
-      <ListGroupGoeun
-        items={items}
-        colors={colors}
-        heading1="Kim Go Eun"
-        heading2="Series"
-        onSelectItem={(item: string) => console.log(item)}
-      />
-    </div>
+    <Alert>
+      Hello <span>World</span>
+    </Alert>
   );
 }
 

52c722f70240eaa5d7fc2b175edc9402923d5997
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-21 14:48:41 +0900

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,28 @@
 function ListGroup() {
   const items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
 
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>
+  //     </>
+  //   );
+
+  // const message = items.length ===0 ? <p>No item found</p> : null;
+
+  // const getMessage = () => items.length ===0 ? <p>No item found</p> : null;
+
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
         ))}
       </ul>
     </>

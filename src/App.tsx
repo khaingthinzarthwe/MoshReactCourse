8910b2ccc69b073de74f050e0dacf1c8b5a49319
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-22 15:39:06 +0900

    Showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+
 function App() {
+  const [ alertVisible, setAlertVisibility ] = useState(false);
   return (
     <div>
-      <Button color="success" onClick={() => console.log("Clicked")}>My Button</Button>
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert> }
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }

5c9726a897caff8d80212555aea37eb63cb7133f
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-22 16:46:43 +0900

    Styling components- Vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 import { useState } from "react";
 
 function App() {
-  const [ alertVisible, setAlertVisibility ] = useState(false);
+  let items = ["New York", "Los Angeles", "San Francisco"];
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert> }
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <ListGroup
+        items={items}
+        heading="Miami"
+        onSelectItem={(item: string) => console.log(item)}
+      />
     </div>
   );
 }

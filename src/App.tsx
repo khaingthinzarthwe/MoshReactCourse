3e63237626df92d07454f74059d9f9afbf84ca58
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-24 15:21:57 +0900

    Exercise-Updating State-Cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discout: .1,
+    items: [
+      {id: 1, title: 'Product 1', quantity: 1},
+      {id: 2, title: 'Product 2', quantity: 1}
+    ]
   });
 
   const handleClick = () => {
-    // pizza.toppings.push("Cheese");
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
-    console.log(pizza);
+    setCart({...cart, items: cart.items.map(item => item.id === 1 ? { ...item, quantity: item.quantity+1 } : item)})
+    console.log(cart);
   };
 
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}>Confirm</button>
     </div>
   );
 }

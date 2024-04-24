a5cba550ebab7ad7b2f8580a3fd373b0760c1915
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-24 21:12:26 +0900

    Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import Message from "./Message";
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: 'Americano',
+    price: 5
+  })
+
+  const handlleClick = () => {
+    // drink.price = 6;
+    // const newDrink = {
+    //   title: drink.title,
+    //   price: 6
+    // }
+    setDrink({ ...drink, price: 6 });
+  }
+
   return (
     <div>
-     <Message />
+      {drink.price}
+      <button onClick={handlleClick}>Click Me</button>
     </div>
   );
 }

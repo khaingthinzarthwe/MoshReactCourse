756cf07ae094595239988886e3d2836d562c9da0
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-26 18:40:06 +0900

    Building a Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi optio
-        quis rerum. Eligendi veniam perferendis ab, ad est repellat. Ipsum
-        repudiandae debitis dolore rem nulla a amet saepe id, neque atque
-        corporis. Perferendis aut eos ducimus odio, ut asperiores consectetur
-        magni quo error temporibus, ipsa nihil quibusdam corrupti ab iste
-        recusandae natus, inventore iure cupiditate hic eum obcaecati commodi
-        culpa. Quos mollitia laboriosam sequi ipsum placeat sunt possimus fugiat
-        minus, labore assumenda, est, veritatis facere animi? Asperiores cum
-        officiis at magnam reiciendis, exercitationem, excepturi provident quod
-        veritatis porro eveniet? Deserunt beatae nobis ducimus maiores sed
-        repellat illum saepe aliquam quod.f
-      </ExpandableText>
+      <Form />
     </div>
   );
 }

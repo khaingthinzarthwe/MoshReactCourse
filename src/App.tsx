66f108354b365317b6c326c04294b76e3e5b44ec
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-04-30 17:19:23 +0900

    Exercise-building a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import Form from "./components/Form";
+import FormExercise from "./components/FormExercise";
 
 function App() {
 
   return (
     <div>
-      <Form />
+      <FormExercise />
     </div>
   );
 }
---
+++ src/components/FormExercise.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const FormExercise = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="desc" className="form-label">
+          Description
+        </label>
+        <input id="desc" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amt" className="form-label">
+          Amount
+        </label>
+        <input id="amt" type="number" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="ctg" className="form-label">
+          Category
+        </label>
+        <select className="form-control" id="ctg">
+          <option></option>
+          <option>Groceries</option>
+          <option>Utilities</option>
+          <option>Entertainment</option>
+        </select>
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default FormExercise;
